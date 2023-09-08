bc8dfead0781248e0ed7218fe989cae7d79e6458
kevio33 <894640176@qq.com>
2023-09-08 21:11:08 +0800

    '更新处理图片逻辑'

--- kevin/handlePic.py
+++ kevin/handlePic.py
@@ -8,17 +8,23 @@ import cv2
 import numpy as np
 '''
     用到easyOCR
-    路径包含中文会乱码
+    路径包含中文会乱码，因此将路径转换为numpy格式
 '''
 def handleJPGorPNG(fileName,filePath):
     # print(filePath,sep=",")
     cv_img= cv2.imdecode(np.fromfile(filePath,dtype=np.uint8),-1)#先转换格式为np，否则中文乱码读不到文件
     reader = easyocr.Reader(['ch_sim','en'],gpu=False) # this needs to run only once to load the model into memory
     result = reader.readtext(cv_img,detail=0) 
-    print(result)
+    dic = {}
+    dic[fileName] = result
+    # print(dic)
+    file = open(fileName+'.txt',"w",encoding='utf-8')
+    file.write(str(dic))
+    file.close()
+
 
 
 
 if __name__ == '__main__':
-    cv_img= cv2.imdecode(np.fromfile('./HCC维护信息.png',dtype=np.uint8),-1)#先转换格式为np，否则中文乱码读不到文件
-    handleJPGorPNG('carbon.jpg',cv_img)
+    # cv_img= cv2.imdecode(np.fromfile('E:\huaweicup\huaweicup2-RichTextDetc\赛题材料\carbon.jpg',dtype=np.uint8),-1)#先转换格式为np，否则中文乱码读不到文件
+    handleJPGorPNG('carbon.jpg','E:\huaweicup\huaweicup2-RichTextDetc\赛题材料\carbon.jpg')
