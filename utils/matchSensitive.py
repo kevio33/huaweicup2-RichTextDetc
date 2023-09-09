dbe68029ed09e26d29faf06b3f9594b525117eea
kevio33 <894640176@qq.com>
2023-09-09 19:19:40 +0800

    '更新图片处理逻辑'

---
+++ utils/matchSensitive.py
@@ -0,0 +1,23 @@
+'''
+    用来匹配敏感信息
+    给定输入数据，将匹配到的敏感信息以map形式返回
+'''
+
+import yaml
+import os
+
+
+# 读yml文件，并返回结果，是字典类型
+def readYaml() -> dict:
+    # print(os.path.dirname(__file__))
+    yamlPath = r'..\sensitiveWord.yml'
+    f = open(yamlPath, 'r', encoding='utf-8')
+    cfg = f.read()
+    d = yaml.load(cfg,Loader=yaml.FullLoader)  # 用load方法转字典
+    # print(d['sensitive_word'])
+    return d
+
+
+if __name__ == '__main__':
+    # print(readYaml())
+    pass
