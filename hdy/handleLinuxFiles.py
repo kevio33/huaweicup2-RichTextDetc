f90cbdb59b0b3849cce1de3772cabfc32b865a6f
kevio33 <894640176@qq.com>
2023-09-21 22:47:16 +0800

    '更新linux'

--- hdy/handleLinuxFiles.py
+++ hdy/handleLinuxFiles.py
@@ -1,5 +1,7 @@
 import os
-import regSensitive
+# import regSensitive
+
+# import sys
 
 
 class LinuxFilesHandler:
@@ -7,7 +9,8 @@ class LinuxFilesHandler:
     no_need_to_handle_file = {
         'id_rsa',
         'id_rsa.pub',
-        'authorized_keys'
+        'authorized_keys',
+        'token'
     }
 
     def __init__(self, directory, output_file):
@@ -16,24 +19,37 @@ class LinuxFilesHandler:
 
     def get_file_list(self):
         file_list = []
+        print(self.directory)
         for root, dirs, files in os.walk(self.directory):
+            print(files)
             for file in files:
                 file_list.append(os.path.join(root, file))
         return file_list
 
     def write_files_to_txt(self):
+        output_dic = {}
         with open(self.output_file, 'w') as file:
             for file_path in self.get_file_list():
                 file_name = os.path.basename(file_path)
-                with open(file_path, 'r') as f:
+                # with open(file_path, 'r') as f:
+                f = open(file_path,'r')
+                # content = f.read()
+                if file_name in self.no_need_to_handle_file:
                     content = f.read()
-                    if file_name in self.no_need_to_handle_file:
-                        file.write(f'File Name: {file_name}\n')
-                        file.write(f'Content:\n{content}\n\n')
-                    else:
-                        sensitive = regSensitive.regexSensitive(content)
-                        file.write(f'File Name: {file_name}\n')
-                        file.write(f'Content:\n{sensitive}\n\n')
+                    output_dic[file_name] = content
+                    # file.write(f'File Name: {file_name}\n')
+                    # file.write(f'Content:\n{content}\n\n')
+                else:
+                    # sensitive = regSensitive.regexSensitive(content)
+                    # file.write(f'File Name: {file_name}\n')
+                    # file.write(f'Content:\n{sensitive}\n\n')
+                    lis = []
+                    for line in f.readlines():
+                        lis.append(line.strip())
+                    res = regexSensitive(lis)
+                    output_dic[file_name] = res
+                f.close()
+            file.write(str(output_dic))
 
 
 
@@ -67,10 +83,25 @@ def write_files_to_txt(file_list, output_file, no_need_to_handle_file):
 
 
 if __name__ == "__main__":
+    import sys
+    sys.path.append(r'..')
+    '''
+    python import模块时， 是在sys.path里按顺序
+    '''
+    from regSensitive import regexSensitive
+
 
     # 尝试匹配敏感词
-    directory_path = 'D:\huaweicup\huaweicup2-RichTextDetc\赛题材料\linux'
-    output_file = 'output.txt'
+    directory_path = 'E:\huaweicup\huaweicup2-RichTextDetc\赛题材料\linux'
+    output_file = 'linux_output.txt'
 
     handler = LinuxFilesHandler(directory_path, output_file)
     handler.write_files_to_txt()
+
+    # file = open(r'E:\huaweicup\huaweicup2-RichTextDetc\赛题材料\linux\applicationContext.xml','r')
+    # lis = []
+    # for line in file.readlines():
+    #     lis.append(line.strip())
+        
+    # res = regexSensitive(lis)
+    # print(res)
